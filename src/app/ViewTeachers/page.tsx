56d38a3be92b000997c8f0171815022d6f25154a
apnaadmin <apnividyainfo@gmail.com>
2024-01-10 11:42:13 +0530

    done minor changes

--- src/app/ViewTeachers/page.tsx
+++ src/app/ViewTeachers/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import Image from 'next/image'
-import { MapPin,Brain,Microscope} from 'lucide-react'
-import Filter from '../components/filter'
+import React from "react";
+import Image from "next/image";
+import { MapPin, Brain, Microscope } from "lucide-react";
+import Filter from "../components/filter";
 import {
   Card,
   CardContent,
@@ -9,140 +9,154 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "shad/card"
-import { getAllUsers } from '@/src/actions/user.action'
-import { Button } from '@nextui-org/react'
-import Search from '../components/Search'
-import { useSearchParams,useRouter,usePathname } from 'next/navigation'
-import ModalButton from '../components/ModalButton'
-import MaxWidthWrapper from '../components/MaxWidthWrapper'
-import FilterScreen from '../components/filterScreen'
-const page = async({searchParams}:any) => {
-    try{
-  
+} from "shad/card";
+import { getAllUsers } from "@/src/actions/user.action";
+import { Button } from "@nextui-org/react";
+import Search from "../components/Search";
+import { useSearchParams, useRouter, usePathname } from "next/navigation";
+import ModalButton from "../components/ModalButton";
+import MaxWidthWrapper from "../components/MaxWidthWrapper";
+import FilterScreen from "../components/filterScreen";
+const page = async ({ searchParams }: any) => {
+  try {
     const result = await getAllUsers({
       searchQuery: searchParams.name,
-      stateQuery:searchParams.state
-    })
-   
-   
-    
+      stateQuery: searchParams.state,
+    });
+
     return (
       <>
-       <MaxWidthWrapper>
-        <div className='flex flex-col justify-center items-center'>
-            
-            <div className='flex max-sm:flex-col sm:gap-5 items-center justify-center'>
-                <div className=' '>
-      <Search
-  iconPosition='left'
-  otherClasses='flex items-center justify-center mb-8'  
-  placeholder='Search anything'
-  route='/ViewTeachers'
-/>
-</div>
-<div>
-
-<FilterScreen/>
-</div>
-</div>
-
-<div>
-      {result.map((item, i) => (
-  <div key={i}>
-   
-
-      <div className='bg-white m-3 px-3 pt-3 pb-2 my-20 shadow-courses rounded-2xl sm:grid sm:grid-cols-2 dark:bg-gray-300 '>
-          <div className="relative rounded-3xl">
-              <Image src="https://nextui.org/images/hero-card.jpeg" alt="gaby" width={238} height={12} className=" mt-3 border-2 rounded-lg border-gray-500 dark:bg- "  />
-          
-              <div className="absolute right-5 -bottom-2 bg-ultramarine rounded-full p-6">
-               
+        <MaxWidthWrapper>
+          <div className="flex flex-col justify-center items-center">
+            <div className="flex max-sm:flex-col sm:gap-5 items-center justify-center">
+              <div className=" ">
+                <Search
+                  iconPosition="left"
+                  otherClasses="flex items-center justify-center mb-8"
+                  placeholder="Search anything"
+                  route="/ViewTeachers"
+                />
               </div>
+              <div>
+                <FilterScreen />
+              </div>
+            </div>
+
+            <div>
+              {result.map((item, i) => (
+                <div key={i}>
+                  <div className="bg-white m-3 px-3 pt-3 pb-2 my-20 shadow-courses rounded-2xl sm:grid sm:grid-cols-2 dark:bg-gray-300 ">
+                    <div className="relative rounded-3xl">
+                      <Image
+                        src="https://nextui.org/images/hero-card.jpeg"
+                        alt="gaby"
+                        width={238}
+                        height={12}
+                        className=" mt-3 border-2 rounded-lg border-gray-500 dark:bg- "
+                      />
+
+                      <div className="absolute right-5 -bottom-2 bg-ultramarine rounded-full p-6"></div>
+                    </div>
+
+                    <div className="px-3">
+                      <h4 className="text-2xl font-semibold pt-6 text-gray-700">
+                        {item.name},
+                      </h4>
+                      <h4 className="text-lg  pt-1 text-gray-500">
+                        {item.experience}
+                      </h4>
+
+                      <hr style={{ color: "#C4C4C4" }} />
+                      <div className="grid grid-cols-1 ml-3">
+                        <div className="flex justify-between pt-6">
+                          <div className="flex ">
+                            <Image
+                              src={"/assets/courses/book-open.svg"}
+                              alt="users"
+                              width={24}
+                              height={24}
+                              className="inline-block m-auto"
+                            />
+                            <h3 className="text-base font-medium text-black opacity-75">
+                              <span className="text-blue-600 font-semibold">
+                                Subject:
+                              </span>
+                              {item.subject}
+                            </h3>
+                          </div>
+                        </div>
+                        <div className="flex justify-between pt-6">
+                          <div className="flex ">
+                            <Image
+                              src={"/assets/courses/users.svg"}
+                              alt="users"
+                              width={24}
+                              height={24}
+                              className="inline-block m-auto"
+                            />
+                            <h3 className="text-base font-medium text-black opacity-75">
+                              <span className="text-blue-600 font-semibold">
+                                Mode:
+                              </span>
+                              {item.mode ? item.mode : "Offline"}
+                            </h3>
+                          </div>
+                        </div>
+                        <div className="flex justify-between pt-6">
+                          <div className="flex ">
+                            {/* <Image src={'/assets/courses/users.svg'} alt="users" width={24} height={24} className="inline-block m-auto" /> */}
+                            <MapPin className="inline-block m-auto " />
+                            <h3 className="text-base font-medium text-black opacity-75">
+                              {" "}
+                              <span className="text-blue-600 font-semibold">
+                                Location:
+                              </span>
+                              {item.location}
+                            </h3>
+                          </div>
+                        </div>
+                        <div className="flex justify-between pt-6">
+                          <div className="flex ">
+                            <Image
+                              src={"/assets/courses/book-open.svg"}
+                              alt="users"
+                              width={24}
+                              height={24}
+                              className="inline-block m-auto"
+                            />
+                            <h3 className="text-base font-medium text-black opacity-75">
+                              <span className="text-blue-600 font-semibold">
+                                Contact:
+                              </span>
+                              {item.number}
+                            </h3>
+                          </div>
+                        </div>
+                        <div className="flex flex-col items-center justify-center mt-2">
+                          <ModalButton
+                            name={item.name}
+                            bio={item.bio}
+                            email={item.email}
+                            number={item.number}
+                            location={item.location}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-
-          <div className="px-3">
-              <h4 className='text-2xl font-semibold pt-6 text-gray-700'>{item.name},</h4>
-              <h4 className='text-lg  pt-1 text-gray-500'>{item.experience}</h4>
-              
-            
-           
-           
-              <hr style={{ color: "#C4C4C4" }} />
-              <div className='grid grid-cols-1 ml-3'>
-              <div className="flex justify-between pt-6">
-                  <div className="flex ">
-                      <Image src={'/assets/courses/book-open.svg'} alt="users" width={24} height={24} className="inline-block m-auto" />
-                      <h3 className="text-base font-medium text-black opacity-75"><span className='text-blue-600 font-semibold'>Subject:</span>{item.subject}</h3>
-                  </div>
-                  
-                  
-               
-              </div>
-              <div className="flex justify-between pt-6">
-                 
-                  <div className="flex ">
-                      <Image src={'/assets/courses/users.svg'} alt="users" width={24} height={24} className="inline-block m-auto" />
-                      <h3 className="text-base font-medium text-black opacity-75"><span className='text-blue-600 font-semibold'>Mode:</span>
-                      {item.mode ? item.mode : "Offline"}
-</h3>
-                  </div>
-                  
-               
-              </div>
-              <div className="flex justify-between pt-6">
-                 
-                 <div className="flex ">
-                     {/* <Image src={'/assets/courses/users.svg'} alt="users" width={24} height={24} className="inline-block m-auto" /> */}
-                     <MapPin className="inline-block m-auto " />
-                     <h3 className="text-base font-medium text-black opacity-75"> <span className='text-blue-600 font-semibold'>Location:</span>{item.location}</h3>
-                 </div>
-                 
-              
-             </div>
-                <div className="flex justify-between pt-6">
-                  <div className="flex ">
-                      <Image src={'/assets/courses/book-open.svg'} alt="users" width={24} height={24} className="inline-block m-auto" />
-                      <h3 className="text-base font-medium text-black opacity-75"><span className='text-blue-600 font-semibold'>Contact:</span>{item.number}</h3>
-                  </div>
-                 
-                  
-               
-              </div>
-              <div className='flex flex-col items-center justify-center mt-2'>
-               <ModalButton name={item.name} bio={item.bio} email={item.email} number={item.number} location={item.location}/>
-              </div>
-              </div>
-
-      
-          </div>
-        
-      </div>
-      
-  </div>
-
-
-))}
-</div>
-</div>
-</MaxWidthWrapper>
+        </MaxWidthWrapper>
       </>
     );
-    
-    }
-    catch(error)
-    {
-        console.log(error);
-    }
-  
-  
-  
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export default page
-
-
-
+export default page;
 
 // Bhavna Debnath
 
--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -1,60 +1,140 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Banner = () => {
-
-    return (
-        <div id="home-section" className='bg-lightkblue'>
-            <div className="mx-auto max-w-7xl pt-20 sm:pb-24 px-6">
-
-                <div className='grid grid-cols-1 lg:grid-cols-12 space-x-1'>
-
-                    <div className='col-span-6 flex flex-col justify-evenly'>
-                        <div className='flex gap-2 mx-auto lg:mx-0'>
-                   <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-<path d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M27.5 5L15 17.5125L11.25 13.7625" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>                        
-                        </div>
-                        <h1 className='text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-120 pt-5 lg:pt-0'>
-
-
-                        Find The Perfect Teacher For You
-                        </h1>
-                        <h3 className='text-charcoal text-lg font-normal text-center lg:text-start opacity-75 pt-5 lg:pt-0'>Be the best by learning from the best, elevate your career to new heights of success, paving the way for a brighter future.</h3>
-
-
-                        <div className='flex items-center justify-between pt-10 lg:pt-4'>
-                            <div className='flex gap-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-<path d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M27.5 5L15 17.5125L11.25 13.7625" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>                          <p className='text-sm sm:text-lg font-normal text-black'>A+ Communication Skills</p>
-                            </div>
-                            <div className='flex gap-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-<path d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M27.5 5L15 17.5125L11.25 13.7625" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>                         <p className='text-sm sm:text-lg font-normal text-black'>In-depth Knowledge</p>
-                            </div>
-                            <div className='flex gap-2'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
-<path d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-<path d="M27.5 5L15 17.5125L11.25 13.7625" stroke="#1CBEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>                       <p className='text-sm sm:text-lg font-normal text-black'>1+ year experience</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className='col-span-6 flex justify-center'>
-                        <Image src="/assets/banner/mahila.png" alt="nothing" width={1000} height={805} />
-                    </div>
-                </div>
+  return (
+    <div id="home-section" className="bg-lightkblue">
+      <div className="mx-auto max-w-7xl pt-20 sm:pb-24 px-6">
+        <div className="grid grid-cols-1 lg:grid-cols-12 space-x-1">
+          <div className="col-span-6 flex flex-col justify-evenly">
+            <div className="flex gap-2 mx-auto lg:mx-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 30 30"
+                fill="none"
+              >
+                <path
+                  d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495"
+                  stroke="#1CBEEE"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M27.5 5L15 17.5125L11.25 13.7625"
+                  stroke="#1CBEEE"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
             </div>
+            <h1 className="text-midnightblue text-4xl sm:text-5xl font-semibold text-center lg:text-start lh-120 pt-5 lg:pt-0">
+              Find The Perfect Teacher For You
+            </h1>
+            <h3 className="text-charcoal text-lg font-normal text-center lg:text-start opacity-75 pt-5 lg:pt-0">
+              Be the best by learning from the best, elevate your career to new
+              heights of success, paving the way for a brighter future.
+            </h3>
 
+            <div className="flex gap-3 flex-col sm:flex-row items-center justify-between pt-10 lg:pt-4">
+              <div className="flex gap-2 ml-8">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                >
+                  <path
+                    d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M27.5 5L15 17.5125L11.25 13.7625"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>{" "}
+                <p className="text-sm sm:text-lg font-normal text-black">
+                  A+ Communication Skills
+                </p>
+              </div>
+              <div className="flex gap-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                >
+                  <path
+                    d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M27.5 5L15 17.5125L11.25 13.7625"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>{" "}
+                <p className="text-sm sm:text-lg font-normal text-black">
+                  In-depth Knowledge
+                </p>
+              </div>
+              <div className="flex gap-2 mr-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                >
+                  <path
+                    d="M27.5 13.85V15C27.4985 17.6955 26.6256 20.3183 25.0117 22.4772C23.3977 24.6362 21.1291 26.2156 18.5442 26.9798C15.9593 27.7441 13.1966 27.6523 10.6681 26.7182C8.1396 25.784 5.98082 24.0576 4.5137 21.7963C3.04658 19.535 2.34974 16.86 2.5271 14.1703C2.70445 11.4807 3.74651 8.92036 5.49785 6.87129C7.24919 4.82223 9.61598 3.39418 12.2452 2.80014C14.8745 2.20609 17.6253 2.47787 20.0875 3.57495"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M27.5 5L15 17.5125L11.25 13.7625"
+                    stroke="#1CBEEE"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>{" "}
+                <p className="text-sm sm:text-lg font-normal text-black">
+                  1+ year experience
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-6 flex justify-center">
+            <Image
+              src="/assets/banner/mahila.png"
+              alt="nothing"
+              width={1000}
+              height={805}
+            />
+          </div>
         </div>
-
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
